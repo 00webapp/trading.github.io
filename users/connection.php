54b4f92b53a1d94b4a47f59ca70a0fed83a7ff3b
00webapp <106580774+00webapp@users.noreply.github.com>
2022-05-31 13:23:58 +0530

    Update connection.php

--- users/connection.php
+++ users/connection.php
@@ -1,5 +1,5 @@
 <?php
-$host = "localhost"; //IP of your database
+$host = "103.21.58.50"; //IP of your database
 $userName = "crmbuxco_trading"; //Username for database login
 $userPass = "Kscpl@2011"; //Password associated with the username
 $database = "crmbuxco_trading"; //Your database name
